71f0a7225f8ad459c9ff40d14538cd89dc9ccd21
washwalk <patrickhadow@gmail.com>
2026-01-04 17:42:17 +0000

    Increase logo size to 80px (w-20 h-20)

--- components/Header.tsx
+++ components/Header.tsx
@@ -37,7 +37,7 @@ export default function Header() {
     <header className="fixed top-0 left-0 right-0 z-10 bg-white shadow">
       <div className="container mx-auto px-4 py-2 flex justify-between items-center">
         <Link href="/" className="flex items-center">
-          <div className="relative w-14 h-14">
+          <div className="relative w-20 h-20">
             <Image
               src="/og.png"
               alt="gsyrocks"
